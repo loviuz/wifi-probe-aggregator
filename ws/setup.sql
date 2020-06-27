cd5e7cfa30997db3afd60fb9970da7c91e6fd561
loviuz <loviuz@gmail.com>
2020-06-27 17:30:23 +0200

    Aggiunta struttura database

---
+++ ws/setup.sql
@@ -0,0 +1,13 @@
+--
+-- Creazione tabella per il salvataggio dei mac address
+--
+
+CREATE TABLE `logs` (
+  `id` int(11) NOT NULL,
+  `mac` varchar(17) NOT NULL,
+  `ssid` varchar(255) NOT NULL,
+  `dbm` tinyint(4) NOT NULL,
+  `latitude` decimal(9,6) NOT NULL,
+  `longitude` decimal(9,6) NOT NULL,
+  `received_at` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB;
