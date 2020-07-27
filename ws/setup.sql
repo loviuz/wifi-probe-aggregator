a69d30826490ff8a164e16333ed34fab9b34700e
loviuz <loviuz@gmail.com>
2020-07-27 21:55:26 +0200

    Aggiunta tabella con lista device per mappatura mac address

--- ws/setup.sql
+++ ws/setup.sql
@@ -11,3 +11,11 @@ CREATE TABLE `logs` (
   `longitude` decimal(9,6) NOT NULL,
   `received_at` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB;
+
+-- Tabella per il match fra mac address e nome
+CREATE TABLE `devices` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `nome` VARCHAR(255) NOT NULL,
+  `mac` VARCHAR(17) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE = InnoDB;
